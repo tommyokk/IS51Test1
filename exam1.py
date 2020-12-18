0c959ff1e0e74a1c17330048501d097edeca390b
tommyokk <70242731+tommyokk@users.noreply.github.com>
2020-12-17 16:36:34 -0800

    Update exam1.py

--- exam1.py
+++ exam1.py
@@ -3,12 +3,16 @@
 
 
 """
-We are trying to figure out which of these two salary options 
-will make you more money.
-Option one will make you 100 hundred dollars per day.
-Option 2 will start of with one dollar and double every day.
-Determine which option will pay you more. 
-Use functions named option 1 and 2 to 
-calculate the money earned.
+if "option1" > "option2"
+    print response
+        "Option 1 is better"
+
+if "option2" > "option1"
+    print response 
+        "option 2 is better"
+
+if "option1" = "option2"
+    print response 
+        "option1" and "option2" pays the same
 
 """
